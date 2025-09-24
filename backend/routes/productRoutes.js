c9a3ff89abcd9bcfbb4468ba2d2ff4a10abd4b14
nrspoorthy <nrspoorthy@gmail.com>
2025-09-24 17:58:45 +0530

    Your commit message

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,14 +3,18 @@ const router = express.Router();
 const { getProducts, getProductById,getProductsByCategory,addProduct,deleteAllProducts,addProductsBulk } = require("../controllers/productController");
 
 
+router.get("/", getProducts);
 
+// Put the more specific route BEFORE the dynamic one
+router.get("/category/:category", getProductsByCategory);
 
-router.get("/",getProducts)
-router.get("/category/:category",getProductsByCategory)
-router.get("/:id",getProductById)
+router.get("/:id", getProductById);
+
 router.post("/", addProduct);
 router.post("/bulk", addProductsBulk);
 router.delete("/", deleteAllProducts);
+
+
 
 
 module.exports = router
